8a6a7fa29322f4a490f90feaebc5842fe97af873
root <root@svr-a.gramedia-cloud.net>
2016-10-20 17:13:03 +0700

    doctrine/dbal

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -63,7 +63,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => false,
-            'engine' => null,
+            'engine' => null
         ],
 
         'pgsql' => [
